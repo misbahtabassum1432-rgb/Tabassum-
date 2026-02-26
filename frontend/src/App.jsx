c01b41b238133cb78975d14830f58aad20a0b9d7
misbahtabassum1432-rgb <misbahtabassum1432@gmail.com>
2026-02-26 11:07:02 +0530

    Create main React component for eye detection web app

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Eye Detection Web App</h1>
+            <p>Welcome to the Eye Detection Web Application.</p>
+            {/* Your logic for eye detection will go here */}
+        </div>
+    );
+};
+
+export default App;
